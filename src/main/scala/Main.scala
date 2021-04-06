8b7fac141e1a1f5f750d92391d054ef44bb02a3c
sarpong <abasimisarpong@rocketmail.com>
2021-04-06 23:20:13 +0100

    [SA] val in scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,14 +11,34 @@ object Main {
 
     /** Understanding the difference between functors and Monads */
 
-    val someFood   = List(Some("Banku"), None, Some("Kenkey"), None, Some("Omotuo"))
-    val secondFood = List(None, None, Some("Kenkey"), None, Some("Omotuo"))
+    val someFood    = List(Some("Banku"), None, Some("Kenkey"), None, Some("Omotuo"))
+    val secondFood  = List(None, None, Some("Kenkey"), None, Some("Omotuo"))
+    val someNumbers = List(Some(1), Some(2), None, Some(5))
 
-    val theFunction  : (Option[String]) => String       = (data) => data.get
+//    val theFunction  : (Option[String]) => String       = (data) => data.get // val is eager -> This will not compile
     val theMonadFunc : (Option[String]) => List[Option[String]] = (data) => List(data)
 
-    val theFunctor = Functor[List].map(someFood)(theFunction) // This will cause an error => None.get
+//    val theFunctor = Functor[List].map(someFood)(theFunction) // This will cause an error => None.get
     val theMonad = Monad[List].flatMap(secondFood)(theMonadFunc) // ???
 
+    val answer = Monad[List].flatMap(someNumbers)(eachSome => List(Monoid.combine(eachSome, eachSome)))
+
+    /**
+     * Learning about the Eval Monad and came across this.
+     *
+     * Fun fact.
+     *
+     * Val in scala are eager and memoized.
+     * Memoized means that
+     *
+     * val b = {
+     * println("here")
+     * math.random()
+     * }
+     * when b is called the first time and it returns for example 0.6078846269579766
+     * the next time I call b, it will still return 0.6078846269579766
+     *
+     * sounds crazy right? Try it.
+     * */
   }
 }
